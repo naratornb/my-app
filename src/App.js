98a08971b250dff65b5e0183d37e247926a76aa9
Flook <flook@FLK.local>
2023-08-26 14:01:29 +0700

    Merge with master brranch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import { keyboard } from '@testing-library/user-event/dist/keyboard';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  search = (keyword) => {
+    console.log(keyword)
+    Axios.get('https://google.com').then(result => {
+      console.log(result.data)
+    })
+  }
+  
+  var title = "CodeMobiles"
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <strong>{title}</strong>
+      <table className='NavBar'>
+        <tbody>
+          <tr>
+            <td>
+              <input style={{fontSize: 24, display: 'block', width: '100%', paddingLeft:8}} 
+              placeholder='Enter Your Movies Keyword'
+              onChange={(event) => {this.search(event.target.value)}}/>
+              <img src={require('./assets/logo.svg')} width="50" />
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
+var header = {}
 
 export default App;
